fd12dc010570a2dd120853b154eb5248c6c4f82c
Hans-Jürgen Tappe <hj.tappe@web.de>
2019-09-23 16:34:10 +0200

    Add function to calculate remaining time to next event

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { sprintf } from 'sprintf-js';
 
 import { SpreadsheetDS } from '../data/spreadsheet-data.service';
 import { isUndefined, isBoolean } from 'util';
@@ -16,6 +17,7 @@ export class EventsComponent implements OnInit {
   toggleDescriptionsName: string;
   onlyUpcoming: boolean;
   showDescriptions: boolean;
+  nextEventTimeOffset: string;
 
   constructor(public sds: SpreadsheetDS) {
     this.objName = 'events';
@@ -28,6 +30,7 @@ export class EventsComponent implements OnInit {
         this.events = newData;
       }
     );
+
   }
 
   ngOnInit() {
@@ -35,6 +38,7 @@ export class EventsComponent implements OnInit {
       // use the local storage if there until HTTP call retrieves something
       JSON.parse(localStorage[this.sds.ssIDs.getCacheName(this.objName)] || '[]')
     );
+    this.nextEventTimeOffset = this.getNextEventTimeOffset();
   }
 
   refresh() {
@@ -50,7 +54,7 @@ export class EventsComponent implements OnInit {
     } else {
       this.toggleName = 'Show All';
     }
-    this.onlyUpcoming = ! this.onlyUpcoming;
+    this.onlyUpcoming = !this.onlyUpcoming;
   }
 
   toggleDescriptions(init?: boolean) {
@@ -62,6 +66,43 @@ export class EventsComponent implements OnInit {
     } else {
       this.toggleDescriptionsName = 'Hide Descriptions';
     }
-    this.showDescriptions = ! this.showDescriptions;
+    this.showDescriptions = !this.showDescriptions;
+  }
+
+  getNextEventTimeOffset(): string {
+    let i = 0;
+    let nextEvent: EventInterface;
+    const now = new Date();
+    let then: Date;
+
+    do {
+      nextEvent = this.events[i];
+      then = new Date(nextEvent.Schedule);
+      i++;
+    } while (i < this.events.length && then <= now);
+
+    if (i >= this.events.length) {
+      return 'No event scheduled';
+    } else {
+      let timediff: number = then.getTime() - now.getTime();
+      let negative = '';
+      if (timediff < 0) {
+        negative = '-';
+        timediff = -timediff;
+      }
+
+      const days: number = timediff / (1000 * 60 * 60 * 24);
+      let rest: number = timediff % (1000 * 60 * 60 * 24);
+
+      const hours: number = rest / (1000 * 60 * 60);
+      rest = rest % (1000 * 60 * 60);
+
+      const minutes: number = rest / (1000 * 60);
+      rest = rest % (1000 * 60);
+
+      const seconds = rest / (1000);
+      return 'Next Event in: ' + negative + sprintf('%dd %02d:%02d:%02d', days, hours, minutes, seconds)
+        + ' (at ' + nextEvent.Schedule + ')';
+    }
   }
 }
