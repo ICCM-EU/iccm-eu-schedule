4a4aafa5d0ce7b5cde680c5a23a26355b1148fb7
Hans-Jürgen Tappe <hj.tappe@web.de>
2019-11-15 22:58:05 +0100

    Revert "ADD A SAMPLE CALENDAR VIEW AND UPDATE TO ANGULAR 7"
    This reverts commit 49b8266338e639743085125a9335c18d0d3c2350.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 const appRoutes: Routes = [
+  // { path: '', loadChildren: './countdown-timer/countdown-timer.module#CountdownTimerModule' },
   { path: 'countdown', loadChildren: './countdown-timer/countdown-timer.module#CountdownTimerModule' },
-  { path: 'planner', loadChildren: './planner-view/planner-view.module#PlannerViewModule' },
   { path: '404', loadChildren: './page-not-found/page-not-found.module#PageNotFoundModule' },
   { path: '', loadChildren: './events/events.module#EventsModule' },
   { path: '**', redirectTo: '/404'}
