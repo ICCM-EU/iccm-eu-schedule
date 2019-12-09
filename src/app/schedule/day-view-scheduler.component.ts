85e59ea2d834f7d8bbb04612a36b5d38df8a7e1c
Hans-Jürgen Tappe <hj.tappe@web.de>
2019-12-09 18:19:43 +0100

    Improve height and width in the scheduler view and separate classes into files.

--- src/app/schedule/day-view-scheduler.component.ts
+++ src/app/schedule/day-view-scheduler.component.ts
@@ -1,44 +1,9 @@
-import { Component, EventEmitter, Injectable, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CalendarUtils, CalendarWeekViewComponent } from 'angular-calendar';
-import { WeekView, GetWeekViewArgs } from 'calendar-utils';
-
-// Calculate it from number of user columns
-const EVENT_WIDTH = (150 > (window.innerWidth - 70) / 5) ? 150 : ((window.innerWidth - 70) / 5);
-
-interface DayViewScheduler extends WeekView {
-  users: any[];
-}
-
-@Injectable()
-export class DayViewSchedulerCalendarUtils extends CalendarUtils {
-  getWeekView(args: GetWeekViewArgs): DayViewScheduler {
-    const view: DayViewScheduler = {
-      ...super.getWeekView(args),
-      users: []
-    };
-    view.hourColumns[0].events.forEach(({ event }) => {
-      if (!view.users.find(user => user.name === event.meta.user.name)) {
-        view.users.push(event.meta.user);
-      }
-    });
-
-    // sort the users by their names
-    view.users.sort((user1, user2) => user1.name.localeCompare(user2.name));
-    view.hourColumns[0].events = view.hourColumns[0].events.map(
-      dayViewEvent => {
-        const userObj = view.users.find(entry => entry.name === dayViewEvent.event.meta.user.name);
-        const index = view.users.indexOf(userObj);
-        dayViewEvent.left = index * EVENT_WIDTH; // change the column of the event
-        dayViewEvent.width = EVENT_WIDTH;
-        return dayViewEvent;
-      }
-    );
-    return view;
-  }
-}
+import { DayViewSchedulerInterface } from './dayViewSchedulerInterface';
+import { DayViewSchedulerCalendarUtils } from './dayViewSchedulerCalendarUtils';
 
 @Component({
-  // tslint:disable-line max-classes-per-file
   selector: 'app-day-view-scheduler',
   styles: [
     `
@@ -47,7 +12,7 @@ export class DayViewSchedulerCalendarUtils extends CalendarUtils {
         margin-left: 70px;
       }
       .day-view-column-header {
-        width: ` + EVENT_WIDTH + `px;
+        width: ` + DayViewSchedulerCalendarUtils.getColumnWidth() + `px;
         border: solid 1px black;
         text-align: center;
       }
@@ -70,9 +35,9 @@ export class DayViewSchedulerCalendarUtils extends CalendarUtils {
 export class DayViewSchedulerComponent extends CalendarWeekViewComponent {
   @Output() userChanged = new EventEmitter();
 
-  view: DayViewScheduler;
+  view: DayViewSchedulerInterface;
   daysInWeek = 1;
-  hourSegmentHeight = 30;
+  hourSegmentHeight = DayViewSchedulerCalendarUtils.gethourSegmentHeight();
   dayStartHour = 8;
   // tooltipTemplate = './tooltip.html';
   /**
@@ -85,5 +50,5 @@ export class DayViewSchedulerComponent extends CalendarWeekViewComponent {
    */
   // tooltipDelay: number | null;
 
-  eventWidth = EVENT_WIDTH;
+  eventWidth = DayViewSchedulerCalendarUtils.getColumnWidth();
 }
