ca937dbab0ee4e17ce1ad6081db7b864ea4dca61
Hans-Jürgen Tappe <hj.tappe@web.de>
2020-01-29 23:16:34 +0100

    Refine the crop algorithm and parametrization

--- src/app/data/textManager.ts
+++ src/app/data/textManager.ts
@@ -1,10 +1,17 @@
 export class TextManager {
   static cropDescriptionAfter(what: string, where: number): string {
-    const realWhere = what.indexOf(' ', where);
-    let out = what.substring(0, realWhere);
-    if (out.length !== what.length) {
-      out = out + '...';
+    if (where <= what.length) {
+      let realWhere = what.indexOf(' ', where);
+      if (realWhere <= where) {
+        realWhere = where;
+      }
+      let out = what.substring(0, realWhere);
+      if (out.length !== what.length) {
+        out = out + '...';
+      }
+      return out;
+    } else {
+      return what;
     }
-    return out;
   }
 }
